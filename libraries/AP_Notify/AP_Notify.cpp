d37073664745372ba19a23ec46f5444ca8290a8c
Jonathan Challinger <mr.challinger@gmail.com>
2016-01-22 13:24:10 -0800

    AP_Notify: prefix config defines

--- libraries/AP_Notify/AP_Notify.cpp
+++ libraries/AP_Notify/AP_Notify.cpp
@@ -43,13 +43,13 @@ struct AP_Notify::notify_events_type AP_Notify::events;
     AP_BoardLED boardled;
     ToshibaLED_PX4 toshibaled;
 
-#ifdef SOLO_TONES_ENABLED
+#ifdef AP_NOTIFY_SOLO_TONES_ENABLED
     ToneAlarm_PX4_Solo tonealarm;
 #else
     ToneAlarm_PX4 tonealarm;
 #endif
 
-#ifdef OREOLED_ENABLED
+#ifdef AP_NOTIFY_OREOLED_ENABLED
     OreoLED_PX4 oreoled;
     NotifyDevice *AP_Notify::_devices[] = {&boardled, &toshibaled, &tonealarm, &oreoled};
 #else
