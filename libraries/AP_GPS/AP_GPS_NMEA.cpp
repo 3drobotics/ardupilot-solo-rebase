b7703bff710ab51dbe9eddbcc230bbf20e51072d
Andrew Tridgell <andrew@tridgell.net>
2016-05-05 11:28:35 +1000

    AP_GPS: change ground course to be in degrees
    more accuracy for replay

--- libraries/AP_GPS/AP_GPS_NMEA.cpp
+++ libraries/AP_GPS/AP_GPS_NMEA.cpp
@@ -297,7 +297,7 @@ bool AP_GPS_NMEA::_term_complete()
                     state.location.lat     = _new_latitude;
                     state.location.lng     = _new_longitude;
                     state.ground_speed     = _new_speed*0.01f;
-                    state.ground_course_cd = wrap_360_cd(_new_course);
+                    state.ground_course    = wrap_360(_new_course*0.01f);
                     make_gps_time(_new_date, _new_time * 10);
                     state.last_gps_time_ms = now;
                     // To-Do: add support for proper reporting of 2D and 3D fix
@@ -314,10 +314,10 @@ bool AP_GPS_NMEA::_term_complete()
                     // To-Do: add support for proper reporting of 2D and 3D fix
                     state.status        = AP_GPS::GPS_OK_FIX_3D;
                     break;
-                case _GPS_SENTENCE_GPVTG:
-                    _last_GPVTG_ms = now;
-                    state.ground_speed     = _new_speed*0.01f;
-                    state.ground_course_cd = wrap_360_cd(_new_course);
+                case _GPS_SENTENCE_VTG:
+                    _last_VTG_ms = now;
+                    state.ground_speed  = _new_speed*0.01f;
+                    state.ground_course = wrap_360(_new_course*0.01f);
                     fill_3d_velocity();
                     // VTG has no fix indicator, can't change fix status
                     break;
