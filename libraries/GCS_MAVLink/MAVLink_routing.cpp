7939e7358cb482e15d890c3584b8d3f425d5228e
bugobliterator <siddharthbharatpurohit@gmail.com>
2016-06-24 20:49:25 +0530

    AP_Mount: fix serial port setting for qx1 gimbal

--- libraries/GCS_MAVLink/MAVLink_routing.cpp
+++ libraries/GCS_MAVLink/MAVLink_routing.cpp
@@ -253,7 +253,7 @@ void MAVLink_routing::learn_route(mavlink_channel_t in_channel, const mavlink_me
 */
 void MAVLink_routing::handle_heartbeat(mavlink_channel_t in_channel, const mavlink_message_t* msg)
 {
-    if (msg->compid == MAV_COMP_ID_GIMBAL) {
+    if (msg->compid == MAV_COMP_ID_GIMBAL || msg->compid == MAV_COMP_ID_QX1_GIMBAL) {
         return;
     }
 
